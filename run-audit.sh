8488660731cc059810e5c7cf404d771b86c98a76
Patrick Farrar <patrick@live-9.com>
2019-10-20 11:22:06 -0700

    Update script to save results based on timestamp

--- run-audit.sh
+++ run-audit.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+TIMESTAMP=$(date +%s)
+
 for url in $(cat urls.txt); do
     URL=${url}
     URL_NOPRO=${URL:7}
@@ -7,8 +10,9 @@ for url in $(cat urls.txt); do
     URI=$(echo "$FILEPATH" | tr / . )
 
     echo "$URI"
+    echo "$TIMESTAMP"
 
-    URL_PATH="results/$URI"
+    URL_PATH="results/$URI/$TIMESTAMP/"
     mkdir -p "${URL_PATH}"
 
     lighthouse "${url}" --output=json --output-path="$URL_PATH/report-$URI.json" --output json --output html --view
